4e47573cbc3128b4bb37fd76cbbe82fff2420872
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-03-04 16:26:28 +0800

    console(nest): run -nest g resource UserData

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserDataModule } from './user-data/user-data.module';
 
 @Module({
-  imports: [],
+  imports: [UserDataModule],
   controllers: [AppController],
   providers: [AppService],
 })
