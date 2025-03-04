700a1857e4c9641dc7102c87a2dfba6c6521209b
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-03-04 16:29:08 +0800

    console(nest): run -nest g controller post

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserDataModule } from './user-data/user-data.module';
+import { PostController } from './post/post.controller';
 
 @Module({
   imports: [UserDataModule],
-  controllers: [AppController],
+  controllers: [AppController, PostController],
   providers: [AppService],
 })
 export class AppModule {}
